407d6ad75141d05e1844c30a42561c511e39f2f0
Roland <roland@cloudomation.com>
2022-02-08 11:39:45 +0100

    example config file

---
+++ app/config/config_example.php
@@ -0,0 +1,13 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'YOUR_USERNAME');
+define('DB_PASS', 'YOUR_PASSWORD');
+define('DB_NAME', 'YOUR_DATABASE_NAME');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'https://localhost/cms-lbs4/public');
+// Site Name
+define('SITENAME', 'CMS');
