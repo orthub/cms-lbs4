6964549cbc9244c31263359e3653f2cafa1bfb32
roland.ortner <ortner.roland@outlook.at>
2022-02-23 08:15:20 +0100

    colorscheme added, posts styling

--- cms/views/index.php
+++ cms/views/index.php
@@ -6,27 +6,37 @@
 <body>
   <?php require_once __DIR__ . '/partials/navbar.php' ?>
   <div class="container">
-    <?php require_once __DIR__ . '/../controllers/posts.php' ?>
-    <?php $posts = get_last_five_posts() ?>
-    <div class="content-posts">
-      <?php foreach ($posts as $post) : ?>
-      <div class="post">
-        <div class="post-image">
-          <img src="<?php echo $post['image'] ?>" alt="image for post number <?php echo $post['id'] ?>">
+    <div class="row">
+      <div class="col-8">
+        <?php require_once __DIR__ . '/../controllers/posts.php' ?>
+        <?php $posts = get_last_five_posts() ?>
+        <div class="content-posts">
+          <?php foreach ($posts as $post) : ?>
+          <div class="post">
+            <div class="post-image">
+              <img src="<?php echo $post['image'] ?>" alt="image for post number <?php echo $post['id'] ?>">
+            </div>
+            <div class="content">
+              <h2><?php echo $post['title'] ?></h2>
+              <p><?php echo $post['body'] ?></p>
+              <hr />
+              <small><?php echo $post['first_name'] . ' | ' . $post['created'] ?></small>
+              <form action="/cms/views/post.php" method="GET">
+                <input type="hidden" name="post_id" value="<?php echo $post['id'] ?>">
+                <input type="submit" value="Read more...">
+              </form>
+            </div>
+            <div class="clear-float"></div>
+          </div>
+          <?php endforeach ?>
         </div>
-        <div class="content">
-          <h2><?php echo $post['title'] ?></h2>
-          <p><?php echo $post['body'] ?></p>
-          <small><?php echo $post['first_name'] . ' | ' . $post['created'] ?></small>
-          <form action="/cms/views/post.php" method="GET">
-            <input type="hidden" name="post_id" value="<?php echo $post['id'] ?>">
-            <input type="submit" value="Read more...">
-          </form>
-        </div>
-        <div class="clear-float"></div>
-        <hr />
       </div>
-      <?php endforeach ?>
+      <div class="col-4">
+        <h1>SOFTEC</h1>
+        <p>The Company</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam nesciunt commodi eaque tenetur delectus atque
+          cupiditate fugiat.</p>
+      </div>
     </div>
     <?php require_once __DIR__ . '/partials/footer.php' ?>
   </div>
--- cms/views/partials/footer.php
+++ cms/views/partials/footer.php
@@ -2,3 +2,13 @@
   <a href="/cms/views/about.php">Impressum</a>
   <a href="/cms/views/privacy.php">Privacy</a>
 </div>
+<script>
+function menuBar() {
+  var menu = document.getElementById("navigation");
+  if (menu.className === "menubar") {
+    menu.className += " responsive";
+  } else {
+    menu.className = "menubar";
+  }
+}
+</script>
