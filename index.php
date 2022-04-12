3a10733e3fb2ae3c5d0ff6bdcde9b07a3debd41d
Roland.Ortner <ortner.roland@outlook.at>
2022-04-12 17:55:41 +0200

    path fix

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 // $base_url = dirname(__DIR__);
 // echo $base_url;
 // exit();
-require_once __DIR__ . '/../helpers/session.php';
+require_once __DIR__ . '/helpers/session.php';
 require_once __DIR__ . '/views/main.php';
