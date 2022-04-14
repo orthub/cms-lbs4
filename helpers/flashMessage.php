90805b45e9904830176b14176115e23e85e1fd82
Roland.Ortner <ortner.roland@outlook.at>
2022-04-14 16:40:58 +0200

    flash warning added

--- helpers/flashMessage.php
+++ helpers/flashMessage.php
@@ -4,6 +4,11 @@ if (isset($_SESSION['error']) && !empty($_SESSION['error'])) {
     echo '<p class="error-msg"><b>' . $error . '</b></p>';
   } 
 }
+if (isset($_SESSION['warning']) && !empty($_SESSION['warning'])) {
+  foreach ($_SESSION['warning'] as $warning) {
+    echo '<p class="warning-msg">' . $warning . '</p>';
+  }
+}
 if (isset($_SESSION['success']) && !empty($_SESSION['success'])) {
   foreach ($_SESSION['success'] as $success) {
     echo '<p class="success-msg">' . $success . '</p>';
@@ -11,4 +16,5 @@ if (isset($_SESSION['success']) && !empty($_SESSION['success'])) {
 }
 
 unset($_SESSION['error']);
+unset($_SESSION['warning']);
 unset($_SESSION['success']);
