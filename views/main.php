3a2bb49a752b2830220873a60cf0543f677b3ff0
Roland.Ortner <ortner.roland@outlook.at>
2022-04-18 17:24:06 +0200

    mainpage styling

--- views/main.php
+++ views/main.php
@@ -6,42 +6,56 @@
 <body>
   <?php require_once __DIR__ . '/partials/navbar.php' ?>
   <?php require_once __DIR__ . '/partials/userbar.php' ?>
-  <div class="landing">
-    <div class="landing-img"></div>
-  </div>
   <div class="content">
-    <div class="space-big"></div>
+    <div class="space-big-main"></div>
     <div class="row">
-      <div class="col-4 text-center">
-        <i class="fa-solid fa-pencil main-icons"></i>
-        <p class="main-icon-text">Große Auswahl</p>
+      <div class="col-6">
+        <div class="text-center">
+          <img class="main-img" src="/img/comp/main_3.jpg" alt="main image number three">
+        </div>
       </div>
-      <div class="col-4 text-center">
-        <i class="fa-solid fa-recycle main-icons"></i>
-        <p class="main-icon-text">Recycelte Materialien</p>
-      </div>
-      <div class="col-4 text-center">
-        <i class="fa-solid fa-leaf main-icons"></i>
-        <p class="main-icon-text">Natürliche Materialien</p>
+      <div class="col-6 text-center">
+        <p class="text-bold">Alles Notiert</p>
+        <p>Verwenden sie unsere Produkte für ihre wichtigen Notizen.</p>
       </div>
     </div>
-    <div class="space-big"></div>
+    <div class="space-big-main"></div>
+    <div class="row">
+      <div class="col-6 text-center">
+        <p class="text-bold">Bleiben sie kreativ</p>
+        <p>Mit unseren Bunstiften werden sie eine wahre Freude haben.</p>
+      </div>
+      <div class="col-6">
+        <div class="text-center">
+          <img class="main-img" src="/img/comp/main_2.jpg" alt="main image two">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="space-big-main"></div>
+  <div class="row">
+    <div class="col-4 text-center">
+      <i class="fa-solid fa-pencil main-icons"></i>
+      <p class="main-icon-text">Große Auswahl</p>
+    </div>
+    <div class="col-4 text-center">
+      <i class="fa-solid fa-recycle main-icons"></i>
+      <p class="main-icon-text">Recycelte Materialien</p>
+    </div>
+    <div class="col-4 text-center">
+      <i class="fa-solid fa-leaf main-icons"></i>
+      <p class="main-icon-text">Natürliche Materialien</p>
+    </div>
+  </div>
+  <div class="space-big-main"></div>
   </div>
   <div class="row">
     <div class="spacer-main text-center">
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <h2>Ein Büro ohne Stifte ist trotz Digitalisierung in der heutigen Zeit nicht wegzudenken.</h2>
+      <h2 class="main-mid">Ein Büro ohne Stifte ist trotz Digitalisierung in der heutigen Zeit nicht wegzudenken.</h2>
     </div>
   </div>
   <div class="content">
-    <div class="space-big"></div>
+    <div class="space-big-main"></div>
     <div class="row">
       <div class="col-3 text-center">
         <p class="text-bold">Schnell trocknend</p>
@@ -56,7 +70,7 @@
         <p class="text-bold">Dokumentenecht</p>
       </div>
     </div>
-    <div class="space-big"></div>
+    <div class="space-big-main"></div>
   </div>
   <?php require_once __DIR__ . '/partials/footer.php' ?>
 </body>
